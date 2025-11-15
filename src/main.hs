a2038595becb9283182d52767de9e85ff0b8c1dc
Martin Großer <grosser.martin@hotmail.de>
2025-11-15 11:57:52 +0100

    feat: pass file path, read file and print it out

--- src/main.hs
+++ src/main.hs
@@ -1,4 +1,18 @@
 module Main where
 
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+import System.IO (hPutStrLn, stderr)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  -- getArgs provides IO [String]
+  args <- getArgs
+  case args of
+    [filepath] -> do
+      -- readFile provides IO String
+      content <- readFile filepath
+      putStrLn content
+    _ -> do
+      hPutStrLn stderr "Usage: ff FILEPATH"
+      exitFailure

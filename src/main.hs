df021c7a4770e3878cca148fdc104f76776e4840
Martin Großer <grosser.martin@hotmail.de>
2025-11-15 14:26:20 +0100

    feat: process a path recursively

--- src/main.hs
+++ src/main.hs
@@ -1,18 +1,64 @@
 module Main where
 
+import Control.Monad (unless)
+import Data.List (isPrefixOf)
+import Data.Time (diffUTCTime, getCurrentTime)
+import System.Directory (doesDirectoryExist, doesFileExist, listDirectory, makeAbsolute)
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
+import System.FilePath ((</>))
 import System.IO (hPutStrLn, stderr)
 
 main :: IO ()
 main = do
-  -- getArgs provides IO [String]
+  startTime <- getCurrentTime
   args <- getArgs
   case args of
-    [filepath] -> do
-      -- readFile provides IO String
-      content <- readFile filepath
-      putStrLn content
-    _ -> do
-      hPutStrLn stderr "Usage: ff FILEPATH"
-      exitFailure
+    [path] -> do
+      absolutePath <- makeAbsolute path
+      processPath absolutePath
+    _ -> showUsageAndExit
+  endTime <- getCurrentTime
+  let duration = diffUTCTime endTime startTime
+  putStrLn $ "\nDuration: " ++ show (realToFrac duration * 1000 :: Double) ++ " ms"
+
+processPath :: FilePath -> IO ()
+processPath path
+  | isHidden path = return ()
+  | otherwise = do
+      isFile <- doesFileExist path
+      isDir <- doesDirectoryExist path
+
+      if isFile
+        then handleFile path
+        else
+          if isDir
+            then handleDirectory path
+            else showPathNotFoundError path
+
+showUsageAndExit :: IO ()
+showUsageAndExit = do
+  hPutStrLn stderr "Usage: ff FILEPATH"
+  exitFailure
+
+handleFile :: FilePath -> IO ()
+handleFile path = putStrLn $ "Search in File: " ++ path
+
+handleDirectory :: FilePath -> IO ()
+handleDirectory path = do
+  subpathes <- listDirectory path
+  mapM_
+    ( \subpath -> do
+        unless (isHidden subpath) $
+          processPath $
+            path </> subpath
+    )
+    subpathes
+
+showPathNotFoundError :: FilePath -> IO ()
+showPathNotFoundError path = hPutStrLn stderr $ "Error: Path doesn't exist: " ++ path
+
+isHidden :: FilePath -> Bool
+isHidden path
+  | path == "." = False
+  | otherwise = "." `isPrefixOf` path

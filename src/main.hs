1a56eb25134182c5762a6947fe6361788aec558d
Martin Großer <grosser.martin@hotmail.de>
2025-11-16 13:45:42 +0100

    feat: search in file based on a search pattern

--- src/main.hs
+++ src/main.hs
@@ -1,8 +1,8 @@
 module Main where
 
 import Control.Exception (IOException, try)
-import Control.Monad (unless)
-import Data.ByteString (length, readFile)
+import Control.Monad (unless, when)
+import Data.ByteString.Char8 (ByteString, length, pack, readFile)
 import Data.Char (ord)
 import Data.List (isPrefixOf)
 import Data.Time (diffUTCTime, getCurrentTime)
@@ -10,33 +10,38 @@ import System.Directory (doesDirectoryExist, doesFileExist, listDirectory, makeA
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
 import System.FilePath ((</>))
-import System.IO (hPutStrLn, stderr)
+import System.IO (hPutStrLn, hSetEncoding, stderr, stdout, utf8)
+import Text.Regex.TDFA ((=~))
 
 main :: IO ()
 main = do
+  hSetEncoding stdout utf8
+  hSetEncoding stderr utf8
+
   startTime <- getCurrentTime
   args <- getArgs
   case args of
-    [path] -> do
+    [pattern, path] -> do
       absolutePath <- makeAbsolute path
-      processPath absolutePath
+      let searchPattern = Data.ByteString.Char8.pack pattern
+      processPath searchPattern absolutePath
     _ -> showUsageAndExit
   endTime <- getCurrentTime
   let duration = diffUTCTime endTime startTime
   putStrLn $ "\nDuration: " ++ show (realToFrac duration * 1000 :: Double) ++ " ms"
 
-processPath :: FilePath -> IO ()
-processPath path
+processPath :: Data.ByteString.Char8.ByteString -> FilePath -> IO ()
+processPath pattern path
   | isHidden path = return ()
   | otherwise = do
       isFile <- doesFileExist path
       isDir <- doesDirectoryExist path
 
       if isFile
-        then handleFile path
+        then handleFile pattern path
         else
           if isDir
-            then handleDirectory path
+            then handleDirectory pattern path
             else showPathNotFoundError path
 
 showUsageAndExit :: IO ()
@@ -47,14 +52,18 @@ showUsageAndExit = do
 makeSafe :: String -> String
 makeSafe = map (\c -> if ord c > 127 then '?' else c)
 
-handleFile :: FilePath -> IO ()
-handleFile path = do
-  content <- Data.ByteString.readFile path
-  putStrLn $ "Search in File: " ++ makeSafe path ++ " (" ++ show (Data.ByteString.length content) ++ " bytes)"
-  return ()
+handleFile :: Data.ByteString.Char8.ByteString -> FilePath -> IO ()
+handleFile pattern path = do
+  result <- try (Data.ByteString.Char8.readFile path) :: IO (Either IOException ByteString)
+  case result of
+    Left err -> putStrLn $ "Error: Could not read file: " ++ path ++ " (" ++ show err ++ ")"
+    Right content -> do
+      when (content =~ pattern :: Bool) $
+        putStrLn $
+          "Find in:" ++ makeSafe path ++ " (" ++ show (Data.ByteString.Char8.length content) ++ " bytes)"
 
-handleDirectory :: FilePath -> IO ()
-handleDirectory path = do
+handleDirectory :: Data.ByteString.Char8.ByteString -> FilePath -> IO ()
+handleDirectory pattern path = do
   result <- try $ listDirectory path :: IO (Either IOException [String])
   case result of
     Left err -> putStrLn $ "Error: Could not read directory: " ++ path ++ " (" ++ show err ++ ")"
@@ -62,7 +71,7 @@ handleDirectory path = do
       mapM_
         ( \subpath -> do
             unless (isHidden subpath) $
-              processPath $
+              processPath pattern $
                 path </> subpath
         )
         subpathes

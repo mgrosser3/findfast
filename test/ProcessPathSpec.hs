b6b12d31fb4a09342c27d13876d774711b305b50
Martin Großer <grosser.martin@hotmail.de>
2025-12-05 15:54:41 +0100

    chore: simplify test for processPath

--- test/ProcessPathSpec.hs
+++ test/ProcessPathSpec.hs
@@ -10,32 +10,22 @@ import Test.Hspec
 processPathSpec :: Spec
 processPathSpec = do
   describe "test function processPath" $ do
+    it "print file path" $ do
+      path <- makeAbsolute "./test/data/test-folder-01/01_lorem_40000_words.txt"
+      (output, _) <-
+        capture $
+          processPath printFilePath printDirectoryPath path
+      output `shouldContain` "File: "
+      output `shouldContain` "01_lorem_40000_words.txt"
     it "print pathes of files and directories" $ do
       path <- makeAbsolute "./test/data/test-folder-01"
       (output, _) <-
         capture $
-          processPath printFilePath handleDirectory path
-
-      output `shouldContain` "File: 01_lorem_40000_words.txt"
-      output `shouldContain` "Directory: test-folder-02"
+          processPath printFilePath printDirectoryPath path
+      output `shouldContain` "Directory: "
+      output `shouldContain` "test-folder-01"
   where
     printFilePath :: String -> IO ()
     printFilePath path = putStrLn $ "File: " ++ path
-
     printDirectoryPath :: String -> IO ()
     printDirectoryPath path = putStrLn $ "Directory: " ++ path
-
-    handleDirectory :: String -> IO ()
-    handleDirectory path = do
-      result <- try (listDirectory path) :: IO (Either IOException [String])
-      case result of
-        Left _ -> return ()
-        Right entries ->
-          mapM_
-            ( \entry -> do
-                isFile <- doesFileExist $ path </> entry
-                if isFile
-                  then printFilePath entry
-                  else printDirectoryPath entry
-            )
-            entries
